c69c2d22d45cf10e165b3cf37168e58b67e6439c
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-06 22:52:54 +0100

    Require authentication middleweare

---
+++ middleweare/requireAuth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+import ErrorResponse from '../helpers/errorResponse.js'
+
+const protect = async (req, res, next) => {
+	let token
+
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		try {
+			token = req.headers.authorization.split(' ')[1]
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+			req.user = await User.findById(decoded.id)
+			next()
+		} catch (error) {
+			next(new ErrorResponse('Invalid token - Authorization declined', 403))
+		}
+	} else {
+		next(new ErrorResponse('No Token provided - Authorization declined', 403))
+	}
+}
+
+export default protect
