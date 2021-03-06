b9680e71c9e3e78221ff373532cd61af2c8a90f6
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-06 22:35:30 +0100

    Mount auth routers & error middleweare

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/connectDB.js'
+import errorHandler from './middleweare/errorHandler.js'
+
+// Routes
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config()
 const app = express()
@@ -16,6 +20,11 @@ app.get('/', (req, res) => {
 	res.send('helloo')
 })
 
+// Mount routers
+app.use('/api/v1/auth', authRoutes)
+
+// Error handler
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
 	console.log(
