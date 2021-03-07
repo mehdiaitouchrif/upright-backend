9b1fc5ae732aac6aa50e58c1022b9b7e27d690c7
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-07 21:39:37 +0100

    User routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import {
+	getUsers,
+	getUser,
+	deleteUser,
+	updateUser,
+} from '../controllers/userController.js'
+import protect from '../middleweare/requireAuth.js'
+
+const router = Router()
+
+// Include post resource
+import postRouter from './postRoutes.js'
+router.use('/:userId/posts', postRouter)
+
+router.route('/').get(protect, getUsers)
+router
+	.route('/:id')
+	.delete(protect, deleteUser)
+	.patch(protect, updateUser)
+	.get(protect, getUser)
+
+export default router
