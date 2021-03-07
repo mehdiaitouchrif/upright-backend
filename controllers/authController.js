c25824e3d511e3e35b1d1c1666f348ebfe7e0f40
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-07 21:40:23 +0100

    Change password controller & refactor ownership logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ import ErrorResponse from '../helpers/errorResponse.js'
 import asyncHandler from '../middleweare/asyncHandler.js'
 import User from '../models/User.js'
 
+// @desc	Sign up
+// @route	/api/v1/auth/signup
 export const signUp = asyncHandler(async (req, res, next) => {
 	const user = await User.create(req.body)
 	const token = user.getSignedJwtToken()
@@ -12,6 +14,8 @@ export const signUp = asyncHandler(async (req, res, next) => {
 	})
 })
 
+// @desc	Login
+// @route	/api/v1/auth/login
 export const login = asyncHandler(async (req, res, next) => {
 	const { username, email, password } = req.body
 
@@ -39,4 +43,33 @@ export const login = asyncHandler(async (req, res, next) => {
 		data: user,
 		token,
 	})
+})
+
+// @desc	Change password
+// @route	/api/v1/auth/changepassword
+export const changePassword = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user._id)
+	if (!user) {
+		return next(new ErrorResponse(`No user found`, 404))
+	}
+
+	const { currentPassword, newPassword } = req.body
+	if (!currentPassword) {
+		next(new ErrorResponse('Please enter your current password', 400))
+	}
+	if (!newPassword) {
+		next(new ErrorResponse('Please enter the new password', 400))
+	}
+
+	const isMatch = await user.matchPasswords(currentPassword)
+	if (isMatch) {
+		user.password = newPassword
+		await user.save()
+		res.status(200).json({
+			success: true,
+			data: user,
+		})
+	} else {
+		return next(new ErrorResponse('Incorrect password', 400))
+	}
 })
