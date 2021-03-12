6b865efbaf629f8770c723306491a86ef56494d8
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-12 15:46:20 +0100

    Send emails with Nodemailer

---
+++ helpers/sendEmail.js
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const sendEmail = async (config) => {
+	const transporter = nodemailer.createTransport({
+		service: process.env.SMTP_HOST,
+		auth: {
+			user: process.env.SMTP_EMAIL,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	})
+
+	const message = {
+		from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+		to: config.to,
+		subject: config.subject,
+		html: `
+        <body style="border: 1px solid #ccc; border-radius: 4px; padding: 10px 20px;">
+            <main>
+                <p style="color: #000; font-size: 17px;">${config.greeting}</p>
+                <p style="margin-bottom: 10px; color: #000; font-size: 17px;">${config.message}</p>
+                <a style="display: inline-block; margin: 10px 0; padding: 10px 18px; font-size: 16px; background-color: red; color: white; cursor: pointer; text-decoration: none;" href="${config.url}" target="_blank">${config.urlTitle}</a>
+            </main>
+            <footer style="padding: 16px 0;">
+                <p style="font-size: 14px; color: #666;">Upright &copy; 2021. </p>
+            </footer>
+        </body>
+        `,
+	}
+
+	const res = await transporter.sendMail(message)
+
+	console.log('Message sent', res.messageId)
+}
+
+export default sendEmail
